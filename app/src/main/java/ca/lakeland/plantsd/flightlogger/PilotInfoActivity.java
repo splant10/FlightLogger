1b6ff8848c237dff297b02c1b750864ca223f9f4
Spencer Plant <spencer.plant@lakelandcollege.ca>
2016-05-20 15:36:06 -0600

    can click on a pilot's name

---
+++ app/src/main/java/ca/lakeland/plantsd/flightlogger/PilotInfoActivity.java
@@ -0,0 +1,31 @@
+package ca.lakeland.plantsd.flightlogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PilotInfoActivity extends AppCompatActivity {
+
+    String pilotName;
+    String pilotFlights;
+    String pilotTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilot_info);
+
+        Bundle extras = getIntent().getExtras();
+        pilotName = (String) extras.getSerializable("STRING_PILOT_NAME");
+        pilotFlights = (String) extras.getSerializable("STRING_PILOT_FLIGHTS");
+        pilotTime = (String) extras.getSerializable("STRING_PILOT_TIME");
+
+        TextView textName = (TextView) findViewById(R.id.txtPilotName);
+        TextView textFlights = (TextView) findViewById(R.id.txtPilotFlights);
+        TextView textTime = (TextView)  findViewById(R.id.txtPilotTime);
+
+        textName.setText(pilotName);
+        textFlights.setText(pilotFlights);
+        textTime.setText(pilotTime);
+    }
+}
--- app/src/main/java/ca/lakeland/plantsd/flightlogger/PilotsActivity.java
+++ app/src/main/java/ca/lakeland/plantsd/flightlogger/PilotsActivity.java
@@ -8,12 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class PilotsActivity extends AppCompatActivity {
+public class PilotsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView lvPilots;
     private PilotsAdapter customAdapter;
@@ -41,5 +42,23 @@ public class PilotsActivity extends AppCompatActivity {
         lvPilots = (ListView) findViewById(R.id.lvPilots);
         customAdapter = new PilotsAdapter(this, R.layout.adapter_pilot_row, HomeScreen.getPilotList());
         lvPilots.setAdapter(customAdapter);
+
+        lvPilots.setOnItemClickListener(this);
+
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        //Log.i("Hello THERE", "you clicked item: " + id + " at position: " + position);
+        Pilot pilot = HomeScreen.getPilotList().get(position);
+        //Log.i("-----------------|", "that would be " + pilot.getName());
+        Intent intent = new Intent(this, PilotInfoActivity.class);
+        String pilotName = pilot.getName();
+        String pilotFlights = Integer.toString(pilot.getTakeoffsAndLandings());
+        String pilotTime = Integer.toString(pilot.getFlightTime());
+        intent.putExtra("STRING_PILOT_NAME", pilotName);
+        intent.putExtra("STRING_PILOT_FLIGHTS", pilotFlights);
+        intent.putExtra("STRING_PILOT_TIME", pilotTime);
+
+        startActivity(intent);
     }
 }
