e61f32bf6c49f3ddda2ee32dc5f2976172fa3e8c
Spencer Plant <spencer.plant@lakelandcollege.ca>
2016-06-15 09:02:30 -0600

    can long click as administrator to remove pilot/spotter

--- app/src/main/java/ca/lakeland/plantsd/flightlogger/PilotsActivity.java
+++ app/src/main/java/ca/lakeland/plantsd/flightlogger/PilotsActivity.java
@@ -1,6 +1,8 @@
 package ca.lakeland.plantsd.flightlogger;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,8 +13,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class PilotsActivity extends HomeScreen implements AdapterView.OnItemClickListener {
+public class PilotsActivity extends HomeScreen implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView lvPilots;
     private PilotsAdapter pilotsAdapter;
@@ -48,26 +51,13 @@ public class PilotsActivity extends HomeScreen implements AdapterView.OnItemClic
         pilotsAdapter = new PilotsAdapter(this, R.layout.adapter_pilot_row, stor.getPilots());
         lvPilots.setAdapter(pilotsAdapter);
         lvPilots.setOnItemClickListener(this);
+        lvPilots.setOnItemLongClickListener(this);
 
         lvSpotters = (ListView) findViewById(R.id.lvSpotters);
         spotAdapter = new ArrayAdapter<String>(this, R.layout.adapter_pilot_row, R.id.txtPilotName, stor.getSpotters());
         lvSpotters.setAdapter(spotAdapter);
+        lvSpotters.setOnItemLongClickListener(this);
 
-    }
-
-    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
-        //Log.i("Hello THERE", "you clicked item: " + id + " at position: " + position);
-        Pilot pilot = stor.getPilots().get(position);
-        //Log.i("-----------------|", "that would be " + pilot.getName());
-        Intent intent = new Intent(this, PilotInfoActivity.class);
-        String pilotName = pilot.getName();
-        String pilotFlights = Integer.toString(pilot.getTakeoffsAndLandings());
-        String pilotTime = Integer.toString(pilot.getFlightTime());
-        intent.putExtra("STRING_PILOT_NAME", pilotName);
-        intent.putExtra("STRING_PILOT_FLIGHTS", pilotFlights);
-        intent.putExtra("STRING_PILOT_TIME", pilotTime);
-
-        startActivity(intent);
     }
 
     @Override
@@ -88,4 +78,66 @@ public class PilotsActivity extends HomeScreen implements AdapterView.OnItemClic
         spotAdapter = new ArrayAdapter<String>(this, R.layout.adapter_pilot_row, R.id.txtPilotName, stor.getSpotters());
         lvSpotters.setAdapter(spotAdapter);
     }
+
+
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        //Log.i("Hello THERE", "you clicked item: " + id + " at position: " + position);
+        Pilot pilot = stor.getPilots().get(position);
+        //Log.i("-----------------|", "that would be " + pilot.getName());
+        Intent intent = new Intent(this, PilotInfoActivity.class);
+        String pilotName = pilot.getName();
+        String pilotFlights = Integer.toString(pilot.getTakeoffsAndLandings());
+        String pilotTime = Integer.toString(pilot.getFlightTime());
+        intent.putExtra("STRING_PILOT_NAME", pilotName);
+        intent.putExtra("STRING_PILOT_FLIGHTS", pilotFlights);
+        intent.putExtra("STRING_PILOT_TIME", pilotTime);
+
+        startActivity(intent);
+    }
+
+    public boolean onItemLongClick(AdapterView<?> l, View v, int position, long id) {
+        final int pos = position;
+        final AdapterView<?> av = l;
+
+        // check if admin
+        if (HomeScreen.getAdminLoggedIn()) {
+            // pop up a dialog asking if user wishes to delete the pilot
+
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case DialogInterface.BUTTON_POSITIVE:
+                            // Yes button clicked
+                            if (av.equals(lvPilots)) {
+                                stor.getPilots().remove(pos);
+                                pilotsAdapter.notifyDataSetChanged();
+                                break;
+                            } else if (av.equals(lvSpotters)) {
+                                stor.getSpotters().remove(pos);
+                                spotAdapter.notifyDataSetChanged();
+                                break;
+                            } else {
+                                Toast.makeText(PilotsActivity.this, "well av is weird..", Toast.LENGTH_SHORT).show();
+                                System.out.println("----" + av.toString());
+                                break;
+                            }
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            // No button clicked
+                            break;
+                    }
+                }
+            };
+            AlertDialog.Builder builder = new AlertDialog.Builder(PilotsActivity.this);
+            builder.setMessage("Do you wish to remove this person from the app?")
+                    .setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+
+        }
+
+        return true;
+    }
+
 }
