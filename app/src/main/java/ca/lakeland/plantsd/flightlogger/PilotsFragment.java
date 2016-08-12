a96735d6cd0cfba3940059913c70e557a15e6a0b
Spencer Plant <spencer.plant@lakelandcollege.ca>
2016-08-12 13:15:29 -0600

    preliminary drawer things

---
+++ app/src/main/java/ca/lakeland/plantsd/flightlogger/PilotsFragment.java
@@ -0,0 +1,29 @@
+package ca.lakeland.plantsd.flightlogger;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PilotsFragment extends Fragment {
+
+
+    public PilotsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pilots, container, false);
+    }
+
+}
